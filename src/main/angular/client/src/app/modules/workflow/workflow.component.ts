21d54720c112d41d1b22da703eee86dd4c9ce981
ZTRP0193 <rahul.patidar@zehntech.com>
2026-03-17 14:14:47 +0530

    JOC-2180

--- src/main/angular/client/src/app/modules/workflow/workflow.component.ts
+++ src/main/angular/client/src/app/modules/workflow/workflow.component.ts
@@ -335,6 +335,13 @@ export class BulkAddOrderComponent {
   paths: any = [];
   bulkOrders: any = [];
   orders: any = [];
+  priorities = [
+    {label: 'inventory.label.high', value: 20000},
+    {label: 'inventory.label.aboveNormal', value: 10000},
+    {label: 'inventory.label.normal', value: 0},
+    {label: 'inventory.label.belowNormal', value: -10000},
+    {label: 'inventory.label.Low', value: -20000},
+  ];
 
   dateFormat: any;
   display: any;
@@ -347,7 +354,7 @@ export class BulkAddOrderComponent {
   commonStartDate: any = {fromDate: null, fromTime: null, timeZone: null};
 
   constructor(public coreService: CoreService, private activeModal: NzModalRef,
-    private modal: NzModalService) {
+    private modal: NzModalService, private ref: ChangeDetectorRef, private workflowService: WorkflowService, private cdr: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
@@ -363,7 +370,9 @@ export class BulkAddOrderComponent {
     this.bulkOrders = this.modalData.bulkOrders;
     this.allowEmptyArguments = sessionStorage['allowEmptyArguments'] === 'true';
     this.dateFormat = this.coreService.getDateFormat(this.preferences.dateFormat);
-    this.zones = this.coreService.getTimeZoneList();
+    this.coreService.getTimeZoneList((timezones) => {
+      this.zones = timezones;
+    });
     this.display = this.preferences.auditLog;
     this.comments.radio = 'predefined';
 
@@ -377,6 +386,7 @@ export class BulkAddOrderComponent {
       timeZone: this.preferences.zone,
       at: 'now',
       forceJobAdmission: false,
+      priority: 0,
       tags: [],
       arguments: [],
       startPosition: '',
@@ -434,6 +444,7 @@ export class BulkAddOrderComponent {
         ...ord,
         orderName: this.orders[0].orderId,
         forceJobAdmission: this.orders[0].forceJobAdmission,
+        priority: this.orders[0].priority,
       }
 
       if (this.orders[0].at === 'now' || this.orders[0].at === 'never') {
